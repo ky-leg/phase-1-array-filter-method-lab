30e301de5fff348047e5ae858163765401ee0c63
Kyle Goodwin <kyle.goodwin93@gmail.com>
2021-11-06 18:34:46 -0400

    feels like it took longer than it should have!

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching(array, string){ 
+    let matching = array.filter(function(array){
+        return array.toLowerCase() === string.toLowerCase()
+    })
+    return matching 
+}
+
+function fuzzyMatch(array, match) {
+    let matchingArray = array.filter(function(array){
+        return array[0] === match[0]
+    })
+    return matchingArray
+}
+
+function matchName(array, string) {
+    let matching = array.filter(function(array){
+        return array.name === string;
+    })
+    return matching
+}
+
